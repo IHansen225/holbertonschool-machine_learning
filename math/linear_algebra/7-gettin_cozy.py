84b7116421d0f6ce558343a41eeb5bcd936662df
IHansen225 <hansen.ivan01@gmail.com>
2023-03-20 20:51:58 -0300

    Ex7 commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,6 +4,18 @@
     the added version of the two given matrices.
 """
 
+def matrix_shape(matrix):
+    """
+        Caculates the shape of the provided matrix
+    """
+    dim = [len(matrix)]
+    obj = matrix
+    while True:
+        if isinstance(obj[0], list):
+            dim.append(len(obj[0]))
+            obj = obj[0]
+        else:
+            return dim
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """
@@ -11,8 +23,8 @@ def cat_matrices2D(mat1, mat2, axis=0):
         If the two matrices cannot be
         concatenated, it does nothing.
     """
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
     if axis == 0:
-        nmat = mat1 + mat2
-        return nmat
-    nmat = [mat1[i] + mat2[i] for i in range(len(mat1))]
-    return nmat
+        return mat1 + mat2
+    return [mat1[i] + mat2[i] for i in range(len(mat1))]
