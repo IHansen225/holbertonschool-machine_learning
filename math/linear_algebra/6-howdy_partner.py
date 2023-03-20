65c62f3a3bf1e9386ffb23dce2a29695c0dd4fcd
IHansen225 <hansen.ivan01@gmail.com>
2023-03-20 20:43:43 -0300

    Ex6 commit

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """
-    The cat_arrays(arr1, arr2) function returns
-    the added version of the two given lists.
+    The cat_matrices2D(mat1, mat2) function returns
+    the added version of the two given matrices.
 """
 
 
-def cat_arrays(arr1, arr2):
+def cat_matrices2D(mat1, mat2, axis=0):
     """
-        Adds the two given lists.
+        Adds the two given matrices.
+        If the two matrices cannot be
+        concatenated, it does nothing.
     """
-    return arr1 + arr2
+    if axis == 0:
+        return(mat1 + mat2)
+    return[mat1[i] + mat2[i] for i in range(len(mat1))]
