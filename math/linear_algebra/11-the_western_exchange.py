40827e2088e34994c47390caa0e95823748ac49a
IHansen225 <hansen.ivan01@gmail.com>
2023-03-21 01:56:17 -0300

    Ex11 commit

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -6,8 +6,4 @@
 
 
 def np_transpose(matrix):
-    matrixT = [[] for i in range(len(matrix[0]))]
-    for i in range(len(matrixT)):
-        for j in range(len(matrix)):
-            matrixT[i].append(matrix[j][i])
-    return matrixT
+    return matrix.transpose()
