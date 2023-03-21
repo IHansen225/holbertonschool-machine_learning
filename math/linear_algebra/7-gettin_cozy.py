fca671a0acde8e4090308138ddfc0e442dba8e13
IHansen225 <hansen.ivan01@gmail.com>
2023-03-20 21:13:30 -0300

    Ex7 commit

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -14,5 +14,5 @@ def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
     if axis == 1 and len(mat1) == len(mat2):
-        return [mat1[0] + mat2[0] for i in range(len(mat1))]
+        return [mat1[i] + mat2[i] for i in range(len(mat2))]
     return None
