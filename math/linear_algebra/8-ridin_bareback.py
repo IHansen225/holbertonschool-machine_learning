98d8063dc713448a26cf79de4584d49cc112d230
IHansen225 <hansen.ivan01@gmail.com>
2023-03-21 01:10:30 -0300

    Ex8 commit

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -13,4 +13,10 @@ def mat_mul(mat1, mat2):
     """
     if len(mat1[0]) != len(mat2):
         return None
-    return [[], []]
+    nmat = [[0] * len(mat2[0]) for i in range(len(mat1))]
+    for i in range(len(nmat)):
+        for j in range(len(mat2[0])):
+            nmat[i][j] = 0
+            for k in range(len(mat2)):
+                nmat[i][j] += mat1[i][k] * mat2[k][j]
+    return nmat
