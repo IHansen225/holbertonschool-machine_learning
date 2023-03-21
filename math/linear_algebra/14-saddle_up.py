a6e5301f02eae7134207e50f9a709f5bd4cf3dd0
IHansen225 <hansen.ivan01@gmail.com>
2023-03-21 02:19:54 -0300

    Ex14 commit

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Numpy function to return the
+    multiplied versions of the
+    given matrices.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Multiply the given matrices
+    """
+    return np.matmul(mat1, mat2)
