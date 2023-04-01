d690a9acafb3a0dc21218703f7be8040e90bbbc2
Ihansen225 <ihk225@gmail.com>
2023-03-31 21:50:54 -0300

    Math

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -3,7 +3,20 @@
     Derivative function
 """
 
+
 def poly_derivative(poly):
     """
-        Returns derivative of the given poly
+        Returns the derivative of the given poly
     """
+    if not isinstance(poly, list) or not all(isinstance(c, (int, float)) for c in poly):
+        return None
+    if len(poly) == 1:
+        return [0]
+    derivative = [0] * (len(poly) - 1)
+    for i in range(1, len(poly)):
+        power = i
+        coeff = poly[i]
+        derivative[power - 1] = power * coeff
+    while derivative and derivative[-1] == 0:
+        derivative.pop()
+    return derivative
