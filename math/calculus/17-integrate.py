28c1d095ce6a353ac57128f4ce831f4068fc7f5c
Ihansen225 <ihk225@gmail.com>
2023-03-31 22:11:01 -0300

    Math

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,13 +4,14 @@
 """
 
 
-
 def poly_integral(poly, C=0):
     """
         Returns the integral of the given poly
     """
-    if not isinstance(poly, list) or not all(isinstance(c, (int, float)) for c in poly):
-        return None  # invalid input
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(c, (int, float)) for c in poly):
+        return None
 
     integral = [C]
     for i in range(len(poly)):
