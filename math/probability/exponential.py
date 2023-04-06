2e0a9101d6256d89d93083418a592c118f8a86c0
Ihansen225 <ihk225@gmail.com>
2023-04-06 00:27:28 -0300

    Math

---
+++ math/probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Exponential distribution class
+"""
+
+
+class Exponential():
+    """
+        Exponential distribution object
+    """
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha > 0:
+                self.lambtha = float(lambtha)
+            else:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
