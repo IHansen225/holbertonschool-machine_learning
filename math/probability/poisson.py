2b71c63b4593efe375b8f770f9d498706b68b86f
Ihansen225 <ihk225@gmail.com>
2023-04-05 22:02:54 -0300

    Math

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -3,13 +3,14 @@
     Poisson distribution class
 """
 
+
 class Poisson(object):
     """
         Poisson distribution object
     """
     def __init__(self, data=None, lambtha=1.):
         if data is None:
-            if lambtha < 0.0:
+            if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
