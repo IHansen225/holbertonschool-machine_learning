2e93991485810a37b3da6f636748e374a025c0c2
Ihansen225 <ihk225@gmail.com>
2023-04-07 23:31:17 -0300

    Math

---
+++ math/probability/binomial.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Binomial distribution class
+"""
+
+
+class Binomial():
+    """
+        Binomial distribution object
+    """
+    def __init__(self, data=None, n=1, p=0.5):
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.p = float(p)
+            self.n = int(n)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            
+
+    def factorial(self, n):
+        """
+            Returns the factorial of a given integer n.
+        """
+        if n == 0:
+            return 1
+        else:
+            return n * self.factorial(n-1)
