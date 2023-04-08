08d3df67523565b62a2c6a936d2d2dd9179dc938
Ihansen225 <ihk225@gmail.com>
2023-04-07 23:42:43 -0300

    Math

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -10,7 +10,7 @@ class Binomial():
     """
     def __init__(self, data=None, n=1, p=0.5):
         if data is None:
-            if p < 0 or p > 1:
+            if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
             if n <= 0:
                 raise ValueError("n must be a positive value")
@@ -21,7 +21,14 @@ class Binomial():
                 raise TypeError("data must be a list")
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            
+            m = sum(data) / len(data)
+            res = 0
+            for i in range(len(data)):
+                res += (data[i] - m) ** 2
+            var = res / len(data)
+            self.n = round(m / (-(var/m) + 1))
+            # p == qty successes / qty of trials (n)
+            self.p = m / self.n
 
     def factorial(self, n):
         """
