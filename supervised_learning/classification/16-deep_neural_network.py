7f04ef7f7315fd5af6c22688e3f1715b6c8853f5
Ihansen225 <ihk225@gmail.com>
2023-04-17 22:18:54 -0300

    Deep

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -11,23 +11,23 @@ class DeepNeuralNetwork():
     """
         Deep neural network class.
     """
-    def __init__(self, nx, layers):
+    def __init__(self, nx, ly):
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or not layers:
-            raise TypeError("layers must be a list of positive integers")
-        self.L = len(layers)
-        if not len(list(filter(lambda x: x > 0, layers))) == self.L:
-            raise TypeError("layers must be a list of positive integers")
-        if not len(list(filter(lambda x: isinstance(x, int), layers))) == self.L:
-            raise TypeError("layers must be a list of positive integers")
+        if not isinstance(ly, list) or not ly:
+            raise TypeError("ly must be a list of positive integers")
+        self.L = len(ly)
+        if not len(list(filter(lambda x: x > 0, ly))) == self.L:
+            raise TypeError("ly must be a list of positive integers")
+        if not len(list(filter(lambda x: isinstance(x, int), ly))) == self.L:
+            raise TypeError("ly must be a list of positive integers")
         self.cache = {}
         self.weights = {}
         for l in range(self.L):
             bN = "b{}".format(l + 1)
-            self.weights[bN] = np.zeros((layers[l], 1))
-            nnx = nx if l == 0 else layers[l - 1]
+            self.weights[bN] = np.zeros((ly[l], 1))
+            nnx = nx if l == 0 else ly[l - 1]
             wN = "W{}".format(l + 1)
-            self.weights[wN] = np.random.randn(layers[l], nnx) * np.sqrt(2 / nnx)
+            self.weights[wN] = np.random.randn(ly[l], nnx) * np.sqrt(2 / nnx)
