dd47e87976b35d2feb227fe12aecaa735dc42c70
Ihansen225 <ihk225@gmail.com>
2023-04-19 20:46:24 -0300

    DNN

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -18,13 +18,22 @@ class DeepNeuralNetwork():
             raise ValueError("nx must be a positive integer")
         if not isinstance(ly, list) or not ly:
             raise TypeError("layers must be a list of positive integers")
+        # Number of layers in the neural network.
         self.L = len(ly)
+        # Checks if all the elements in ly are positive integers.
+        # Lambda function to avoid using loops.
         if not len(list(filter(lambda x: x > 0, ly))) == self.L:
             raise TypeError("layers must be a list of positive integers")
+        # Checks if all the elements in ly are integers.
+        # Same thing as above.
         if not len(list(filter(lambda x: isinstance(x, int), ly))) == self.L:
             raise TypeError("layers must be a list of positive integers")
         self.cache = {}
         self.weights = {}
+        # Weight and bias initialization.
+        # The weights are initialized using He et al. method.
+        # The biases are initialized to 0.
+        # The weights are initialized to a random normal distribution.
         for lyr in range(self.L):
             bN = "b{}".format(lyr + 1)
             self.weights[bN] = np.zeros((ly[lyr], 1))
