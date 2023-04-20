2a2b30f8b1a56fead33262c1366c4d1e1534d371
Ihansen225 <ihk225@gmail.com>
2023-04-20 01:56:35 -0300

    TensorFlow

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -10,7 +10,10 @@ def create_layer(prev, n, activation):
     """
         Returns the tensor output of the layer.
     """
+    # Define the initializer to the He et al. method.
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    # Define the layer.
     layer = tf.layers.Dense(units=n, activation=activation,
                             kernel_initializer=init, name="layer")
-    return layer
+    # Return the layer giving it the previous layer as input.
+    return layer(prev)
