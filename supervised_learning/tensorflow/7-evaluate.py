a248e5bd25609b35e28056fba4bf6cdd790bb407
Ihansen225 <ihk225@gmail.com>
2023-04-21 23:14:40 -0300

    Fprop

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Evaluation module.
+"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+        Evaluates the output of a neural network.
+    """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        loss = tf.get_collection('loss')[0]
+        acc = tf.get_collection('accuracy')[0]
+        return sess.run((y_pred, loss, acc), feed_dict={x: X, y: Y})
