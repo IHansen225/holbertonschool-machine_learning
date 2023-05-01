c4a63fa8130cd60bef3355a3295850edf88135ff
Ihansen225 <ihk225@gmail.com>
2023-04-30 23:57:44 -0300

    ?

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Batch Normalization
+"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """
+        Normalizes an unactivated
+        output of a neural network
+    """
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(
+        units=n,
+        kernel_initializer=init,
+        name="layer"
+    )
+    Z = layer(prev)
+    gamma = tf.Variable(
+        tf.constant(1.0, shape=[n]),
+        name="gamma"
+    )
+    beta = tf.Variable(
+        tf.constant(0.0, shape=[n]),
+        name="beta"
+    )
+    mean, var = tf.nn.moments(Z, axes=[0])
+    epsilon = tf.constant(1e-8)
+    Znorm = tf.nn.batch_normalization(
+        x=Z,
+        mean=mean,
+        variance=var,
+        offset=beta,
+        scale=gamma,
+        variance_epsilon=epsilon
+    )
+    return activation(Znorm)
