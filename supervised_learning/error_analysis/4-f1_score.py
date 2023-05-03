ddb8e76e94e14e4ad0d2afc61036de5b91df7f0d
Ihansen225 <ihk225@gmail.com>
2023-05-03 19:43:28 -0300

    Confusion

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -8,6 +8,7 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(cnf):
     """
         Returns an array containing
