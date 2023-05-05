b4ee6a98a480c07824ab111284c43c9d3265c975
Ihansen225 <ihk225@gmail.com>
2023-05-04 22:47:40 -0300

    Normalization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -7,19 +7,14 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """
-        Calculates one pass of gradient
-        descent on the neural network.
-        using the L2 regularization.
-    """
     m = Y.shape[1]
-    dz = cache['A' + str(L)] - Y
-    for i in range(L, 0, -1):
-        A = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
-        b = weights['b' + str(i)]
-        dw = (1 / m) * np.matmul(dz, A.T) + ((lambtha / m) * W)
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        dz = np.matmul(W.T, dz) * (A * (1 - A))
-        weights['W' + str(i)] -= alpha * dw
-        weights['b' + str(i)] -= alpha * db
+    dZ = cache['A' + str(L)] - Y
+    for l in range(L, 0, -1):
+        A_prev = cache['A' + str(l-1)]
+        W = weights['W' + str(l)]
+        b = weights['b' + str(l)]
+        dW = 1./m * np.dot(dZ, A_prev.T) + lambtha/m * W
+        db = 1./m * np.sum(dZ, axis=1, keepdims=True)
+        dZ = np.dot(W.T, dZ) * (1 - np.power(A_prev, 2))
+        weights['W' + str(l)] -= alpha * dW
+        weights['b' + str(l)] -= alpha * db
