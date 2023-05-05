0bb3e5faa8ef6650153973305de80f0663c89626
Ihansen225 <ihk225@gmail.com>
2023-05-04 22:07:28 -0300

    Normalization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -10,6 +10,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
         Returns the L2 cost of a
         neural network
     """
-    res = cost + (lambtha / (2 * m)) * np.sum([np.linalg.norm(w)\
-                                                for w in weights.values()])
+    res = cost + (lambtha / (2 * m)) * np.sum([np.linalg.norm(w)
+                                               for w in weights.values()
+                                               if w is not weights['b' + str(L)]])
     return res
