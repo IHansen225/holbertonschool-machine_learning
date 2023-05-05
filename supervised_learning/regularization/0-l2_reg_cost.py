b12e216787f4ca9de1714d1dd32d617a37c584dd
Ihansen225 <ihk225@gmail.com>
2023-05-04 22:13:55 -0300

    Normalization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -14,5 +14,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     for k, v in weights.items():
         if k[0] == 'W':
             w_new[k] = v
-    res = cost + (lambtha / (2 * m)) * np.sum([np.linalg.norm(w) for w in w_new.values()])
+    res = cost + (lambtha / (2 * m)) * np.sum([np.linalg.norm(w)
+                                               for w in w_new.values()])
     return res
