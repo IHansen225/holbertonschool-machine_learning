12010f2bb9b1b890f80fa576b7a80da45628f035
Ihansen225 <ihk225@gmail.com>
2023-05-04 22:50:35 -0300

    Normalization

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    L2 Regularization gradient
+    descent module.
+"""
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """
+        Returns: a tensor containing the L2 regularization cost.
+    """
+    return cost + tf.losses.get_regularization_losses()
