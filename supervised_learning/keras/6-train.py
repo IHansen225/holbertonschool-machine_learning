b7599be5fd5115ffa6a44d56b092a9e9e0e07a55
Ihansen225 <ihk225@gmail.com>
2023-05-09 23:04:27 -0300

    Keras

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -11,9 +11,10 @@ def train_model(network, data, labels, batch, ep,
     """
         Trains a model using mini-batch gradient descent.
     """
+    cb = None
     if early_stopping and validation_data is not None:
-        cb = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)
+        cb = [K.callbacks.EarlyStopping(monitor='val_loss', patience=patience)]
     return network.fit(data, labels, batch_size=batch, epochs=ep,
                        verbose=verbose, shuffle=shuffle,
                        validation_data=validation_data,
-                       callbacks=[cb] if early_stopping else None)
+                       callbacks=cb)
