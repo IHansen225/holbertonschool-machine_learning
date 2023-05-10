7a56055ca49470c0e1a59c79c31d5aa7706dfae1
Ihansen225 <ihk225@gmail.com>
2023-05-09 21:48:06 -0300

    Keras

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Keras training module.
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
+    """
+        Trains a model using mini-batch gradient descent.
+    """
+    return network.fit(data, labels, batch_size=batch, epochs=ep,
+                       verbose=verbose, shuffle=shuffle,
+                       validation_data=validation_data)
