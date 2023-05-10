a6a357bfec39e4305c39d5de9d322273d25a0d51
Ihansen225 <ihk225@gmail.com>
2023-05-09 22:17:47 -0300

    Keras

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -5,7 +5,8 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
     """
         Trains a model using mini-batch gradient descent.
     """
