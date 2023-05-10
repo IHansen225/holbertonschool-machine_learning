b99c86da511b6709292b636f874107e9b66c77ee
Ihansen225 <ihk225@gmail.com>
2023-05-09 21:33:31 -0300

    Keras

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -6,7 +6,7 @@
 import tensorflow.keras as K
 
 
-def build_model(nx, layers, activations, lambtha, keep_prob):
+def build_model(nx, layers, activations, lam, keep_prob):
     """
         Build Keras model with
         the input object.
@@ -15,7 +15,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                 in each layer of the network.
         activations: list containing the activation
                 functions used for each layer of the network.
-        lambtha: L2 regularization parameter.
+        lam: L2 regularization parameter.
         keep_prob: probability that a node will be kept
                 for dropout regularization.
     """
@@ -23,13 +23,13 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     inputs = K.Input(shape=(nx,))
     # Add first layer with input shape
     x = K.layers.Dense(layers[0], activation=activations[0],
-                       kernel_regularizer=K.regularizers.l2(lambtha))(inputs)
+                       kernel_regularizer=K.regularizers.l2(lam))(inputs)
     # Add subsecuent layers with dropout according to
-    # keep_prob and regularization according to lambtha
+    # keep_prob and regularization according to lam
     for i in range(1, len(layers)):
         x = K.layers.Dropout(1 - keep_prob)(x)
         x = K.layers.Dense(layers[i], activation=activations[i],
-                           kernel_regularizer=K.regularizers.l2(lambtha))(x)
+                           kernel_regularizer=K.regularizers.l2(lam))(x)
     # Create model
     model = K.Model(inputs=inputs, outputs=x)
     return model
