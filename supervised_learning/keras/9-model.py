69f14a72a404d253ef3abc5f94a66909ae53d8ac
Ihansen225 <ihk225@gmail.com>
2023-05-09 23:46:28 -0300

    Keras

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -5,10 +5,11 @@
 import tensorflow.keras as K
 
 
-def save_model(network, filename): 
+def save_model(network, filename):
     """ saves an entire model """
     network.save(filename)
     return None
+
 
 def load_model(filename):
     """ loads an entire model """
