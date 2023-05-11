4c6439793514fcd10c805df38031353ac41fe22c
Ihansen225 <ihk225@gmail.com>
2023-05-11 01:37:11 -0300

    Convolution

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -5,35 +5,25 @@
 import numpy as np
 
 
-def convolve(images, kernels, padding='same', stride=(1, 1)):
+def pool(images, kernel_shape, stride, mode='max'):
     """
     Returns: a numpy.ndarray containing the convolved images.
     """
     # Same as 3-convolve_grayscale.py but with channels
     # Basically same thing but 3D
     m, h, w, c = images.shape
-    kh, kw, kc, nc = kernels.shape
-    ph, pw = 0, 0
-    if isinstance(padding, tuple):
-        ph, pw = padding
-    elif padding == 'same':
-        ph = int(((h - 1) * stride[0] + kh - h) / 2) + 1
-        pw = int(((w - 1) * stride[1] + kw - w) / 2) + 1
-    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                    mode='constant')
-    output_h = int(((h + (2 * ph) - kh) / stride[0]) + 1)
-    output_w = int(((w + (2 * pw) - kw) / stride[1]) + 1)
-    output = np.zeros((m, output_h, output_w, nc))
-    for x in range(nc):
-        kernel = kernels[:, :, :, x]
-        for i in range(output_h):
-            for j in range(output_w):
-                output[:, i, j, x] = (
-                    kernel * images[
-                        :,
-                        i * stride[0]: i * stride[0] + kh,
-                        j * stride[1]: j * stride[1] + kw,
-                        :
-                    ]
-                ).sum(axis=(1, 2, 3))
+    kh, kw = kernel_shape
+    output_h = int((h - kh) / stride[0]) + 1
+    output_w = int((w - kw) / stride[1]) + 1
+    output = np.zeros((m, output_h, output_w))
+    for i in range(output_h):
+        for j in range(output_w):
+            output[:, i, j] = (
+                kernel_shape * images[
+                    :,
+                    i * stride[0]: i * stride[0] + kh,
+                    j * stride[1]: j * stride[1] + kw,
+                    :
+                ]
+            ).sum(axis=(1, 2, 3))
     return output
