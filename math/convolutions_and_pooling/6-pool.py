83289462ffaeda7595297043b68ebb19a4f3dceb
Ihansen225 <ihk225@gmail.com>
2023-05-11 01:43:28 -0300

    Convolution

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-def pool(images, kernel_shape, st, mode='max'):
+def pool(images, kernel_shape, stride, mode='max'):
     """
     Returns: a numpy.ndarray containing the convolved images.
     """
@@ -13,14 +13,15 @@ def pool(images, kernel_shape, st, mode='max'):
     # Basically same thing but 3D
     m, h, w, c = images.shape
     kh, kw = kernel_shape
-    output_h = int((h - kh) / st[0]) + 1
-    output_w = int((w - kw) / st[1]) + 1
+    output_h = int((h - kh) / stride[0]) + 1
+    output_w = int((w - kw) / stride[1]) + 1
     output = np.zeros((m, output_h, output_w, c))
     for i in range(output_h):
         for j in range(output_w):
-                k = images[:, i * st[0]: i * st[0] + kh, j * st[1]: j * st[1] + kw, :]
-                if mode == 'max':
-                    output[:, i, j, :] = np.max(k, axis=(1, 2))
-                elif mode == 'avg':
-                    output[:, i, j, :] = np.mean(k, axis=(1, 2))
+            k = images[:, i * stride[0]: i * stride[0] +
+                       kh, j * stride[1]: j * stride[1] + kw, :]
+            if mode == 'max':
+                output[:, i, j, :] = np.max(k, axis=(1, 2))
+            elif mode == 'avg':
+                output[:, i, j, :] = np.mean(k, axis=(1, 2))
     return output
