86c8f85d12f3521137f7d26dfbbc3802e549fb93
Ihansen225 <ihk225@gmail.com>
2023-05-28 00:51:46 -0300

    Identity block

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -11,8 +11,8 @@ def projection_block(A_prev, filters, s=2):
     """
     F11, F3, F12 = filters
     init = K.initializers.he_normal()
-    conv1 = K.layers.Conv2D(F11, (1, 1), padding='same',
-                            kernel_initializer=init, strides=s)(A_prev)
+    conv1 = K.layers.Conv2D(F11, (1, 1), strides=(s, s), padding='same',
+                            kernel_initializer=init)(A_prev)
     norm1 = K.layers.BatchNormalization()(conv1)
     act1 = K.layers.Activation('relu')(norm1)
     conv2 = K.layers.Conv2D(F3, (3, 3), padding='same',
@@ -22,9 +22,9 @@ def projection_block(A_prev, filters, s=2):
     conv3 = K.layers.Conv2D(F12, (1, 1), padding='same',
                             kernel_initializer=init)(act2)
     norm3 = K.layers.BatchNormalization()(conv3)
-    conv_s = K.layers.Conv2D(F11, (1, 1), padding='same',
-                             kernel_initializer=init, strides=s)(A_prev)
-    norm_s = K.layers.BatchNormalization()(conv_s)
-    add = K.layers.Add()([norm3, norm_s])
+    conv4 = K.layers.Conv2D(F12, (1, 1), strides=(s, s), padding='same',
+                            kernel_initializer=init)(A_prev)
+    norm4 = K.layers.BatchNormalization()(conv4)
+    add = K.layers.Add()([norm3, norm4])
     output = K.layers.Activation('relu')(add)
     return output
