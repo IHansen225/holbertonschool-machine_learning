75b20eb3075894612794fa12110c64f44bd19d43
Ihansen225 <ihk225@gmail.com>
2023-11-05 23:21:31 -0300

    .

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    matrix determinant module
+"""
+
+
+def determinant(matrix):
+    """
+        Returns the determinant of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(row):
+            raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0]
+    det = 0
+    for i, k in enumerate(matrix[0]):
+        rows = [row for row in matrix[1:]]
+        new_m = [[row[n] for n in range(len(matrix)) if n != i] for row in rows]
+        det += k * (-1)**i * determinant(new_m)
+    return det
---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+    matrix minor module
+"""
+
+
+def determinant(matrix):
+    """
+        Returns the determinant of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(row):
+            raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0]
+    det = 0
+    for i, k in enumerate(matrix[0]):
+        rows = [row for row in matrix[1:]]
+        new_m = [[row[n] for n in range(len(matrix)) if n != i] for row in rows]
+        det += k * (-1)**i * determinant(new_m)
+    return det
+
+def minor(matrix):
+    """
+        Returns the minor of matrix
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(row):
+            raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
+        return [[1]]
+    minors = []
+    for i in range(len(matrix)):
+        minors.append([])
+        for j in range(len(matrix)):
+            rows = [matrix[m] for m in range(len(matrix)) if m != i]
+            new_m = [[row[n] for n in range(len(matrix)) if n != j] for row in rows]
+            minors[i].append(determinant(new_m))
+    return minors
