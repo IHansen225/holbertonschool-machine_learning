578e09e982f2d7d276817f65725fcf089c3a7aa2
Ihansen225 <ihk225@gmail.com>
2023-11-05 23:36:57 -0300

    .

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -8,7 +8,9 @@ def adjugate(matrix):
     """
     Returns the matrix adjugate
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+    if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
     n = len(matrix)
     if any(len(row) != n for row in matrix):
@@ -18,8 +20,8 @@ def adjugate(matrix):
     cofactor = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            minor = [row[:j] + row[j + 1 :] 
-                     for row in (matrix[:i] + matrix[i + 1 :])]
+            minor = [row[:j] + row[j + 1:]
+                     for row in (matrix[:i] + matrix[i + 1:])]
             cofactor[i][j] = (-1) ** (i + j) * determinant(minor)
     return transpose(cofactor)
 
@@ -47,4 +49,4 @@ def transpose(matrix):
     Returns the transpose of a matrix
     """
     return [[matrix[j][i] for j in range(len(matrix))]
-             for i in range(len(matrix[0]))]
+            for i in range(len(matrix[0]))]
