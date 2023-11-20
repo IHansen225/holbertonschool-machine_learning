71fa7072a7f459de85803173d92973166f6551c2
Ihansen225 <ihk225@gmail.com>
2023-11-20 00:59:03 -0300

    .

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    PCA module
+"""
+
+
+import numpy as np
+
+
+def pca(X, ndim):
+    """
+    Performs PCA on a dataset
+    """
+    X_centered = X - np.mean(X, axis=0)
+    _, _, vh = np.linalg.svd(X_centered)
+    W = vh.T
+    Wr = W[:, 0:ndim]
+    return X_centered @ Wr
---
+++ unsupervised_learning/dimensionality_reduction/README.md
@@ -0,0 +1,1 @@
+Dimensionality reduction README file
